3fc379bf9a92811928451618509fc448411a7d4c
Shivam Gandhi <shivamgandhi@Shivams-MacBook-Pro.local>
2024-10-07 11:58:49 -0700

    HW2 completed.

---
+++ scripts/Week_2_homework.R
@@ -0,0 +1,19 @@
+set.seed(15)
+hw2 <- runif(50, 4, 50)
+hw2 <- replace(hw2, c(4,12,22,27), NA)
+hw2
+?remove
+?is
+prob1 <- hw2[!is.na(hw2)] #removed the na arguments
+prob1
+times3 <- prob1 * 3
+times3
+plus10 <- times3 + 10
+plus10
+prob1 <- prob1[prob1 >14 & prob1 <38] #Forgot to set between 14 and 38
+times3 <- prob1 * 3 #Multiplies by 3
+times3 #test
+plus10 <- times3 + 10 #Added 10
+plus10 #test
+final <- plus10[c(TRUE, FALSE, TRUE, FALSE)] #Every other number is true false, so it keeps first number and ignores second
+final  #test
