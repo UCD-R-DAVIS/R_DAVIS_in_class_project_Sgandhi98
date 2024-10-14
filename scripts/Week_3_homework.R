23221d386c4cfe89c5f1685cfedd13a5aecc7994
Shivam Gandhi <shivamgandhi@shivams-mbp.lan>
2024-10-14 10:16:17 -0700

    issues with atomic vector? Tried my best, but some weird errors.

---
+++ scripts/Week_3_homework.R
@@ -0,0 +1,22 @@
+surveys <- read.csv("data/portal_data_joined.csv")
+surveys
+surveys_base <- surveys[1:5000]
+surveys_base <- surveys[1:5000, c(6, 9, 13)]
+surveys_base
+surveys_base$species_id <- factor(surveys_base$species_id)
+surveys_base$plot_type <- factor(surveys_base$plot_type)
+surveys_base #Species_id and plot_id now factors
+surveys_base <- surveys_base[!is.na(surveys_base)] #removed NAs
+surveys_base
+challenge_base <- surveys_base[surveys_base[, 2]>150,]
+#Retrying order? 
+surveys <- read.csv("data/portal_data_joined.csv")
+surveys
+surveys_base <- surveys[1:5000, c(6, 9, 13)]
+surveys_base
+surveys_base <- surveys_base[!is.na(surveys_base)] #removed NAs
+surveys_base
+surveys_base$species_id <- factor(surveys_base$species_id)
+surveys_base$plot_type <- factor(surveys_base$plot_type)
+surveys_base
+challenge_base <- surveys_base[surveys_base[,2]>150,]
